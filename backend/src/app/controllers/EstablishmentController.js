c00babe0db1c8afea30d09a117dd3f1812849951
Vitor Araujo <vitoraraujo.dev@gmail.com>
2020-04-24 14:08:04 -0300

    Update de Estabelecimento + Middleware de autenticação

--- backend/src/app/controllers/EstablishmentController.js
+++ backend/src/app/controllers/EstablishmentController.js
@@ -16,6 +16,78 @@ class EstablishmentController {
       okay: true,
     });
   }
+
+  async update(req, res) {
+    const { email, cnpj, oldPassword } = req.body;
+
+    const establishment = await Establishment.findByPk(req.establishmentId);
+
+    if (email !== establishment.email) {
+      const establishmentExists = await Establishment.findOne({
+        where: { email },
+      });
+
+      if (establishmentExists) {
+        return res.status(400).json({ errer: 'E-mail already in use.' });
+      }
+    }
+
+    if (cnpj !== establishment.cnpj) {
+      const establishmentExists = await Establishment.findOne({
+        where: { cnpj },
+      });
+
+      if (establishmentExists) {
+        return res.status(400).json({ errer: 'Establishment already exists.' });
+      }
+    }
+
+    if (oldPassword && !(await establishment.checkPassword(oldPassword))) {
+      return res.status(401).json({ error: 'Old Password does not match.' });
+    }
+
+    await establishment.update(req.body);
+
+    const {
+      id,
+      phone_number,
+      establishment_name,
+      manager_name,
+      manager_lastname,
+      cep,
+      address_number,
+      street,
+      complement,
+      city,
+      state,
+      photo,
+    } = await Establishment.findByPk(req.establishmentId);
+
+    /*
+    {
+      include: [
+        { model: File, as: 'photo', attributes: ['id', 'path', 'url'] },
+      ],
+    }
+    */
+
+    return res.json({
+      id,
+      cnpj,
+      email,
+      phone_number,
+      establishment_name,
+      manager_name,
+      manager_lastname,
+      cep,
+      address_number,
+      street,
+      complement,
+      city,
+      state,
+      photo,
+    });
+  }
 }
 
 export default new EstablishmentController();
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,11 +1,15 @@
 import { Router } from 'express';
 
+import authMiddleware from './app/middlewares/auth';
+
 import SessionController from './app/controllers/SessionController';
 import EstablishmentsController from './app/controllers/EstablishmentController';
 
 const routes = new Router();
 
 routes.post('/sessions', SessionController.store);
+
 routes.post('/establishments', EstablishmentsController.store);
+routes.put('/establishments', authMiddleware, EstablishmentsController.update);
 
 export default routes;
