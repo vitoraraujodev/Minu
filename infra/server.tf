23120b40c1de25e4a2eb2ada782ac5aade127934
yuzobra <yuri.zoel@hotmail.com>
2021-01-09 17:17:34 -0200

    Waits spot request to destroy and complete

--- infra/server.tf
+++ infra/server.tf
@@ -8,7 +8,7 @@ resource "aws_spot_instance_request" "site" {
     instance_type = "t3a.nano"
     iam_instance_profile = aws_iam_instance_profile.site_iam.name
     key_name = "Minu"
-    # availability_zone = "us-east-2b"
+    wait_for_fulfillment=true
     
     network_interface {
       device_index = 0
