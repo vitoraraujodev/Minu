0f87b6e7b9c28dfef09f9231c97361c6c6a511c4
Vitor Araujo <vitoraraujo.dev@gmail.com>
2021-04-25 16:58:28 -0300

    Fix input hour handler

--- frontend/src/util/handleHour.js
+++ frontend/src/util/handleHour.js
@@ -1,6 +1,4 @@
 export default function handleHour(hour) {
-  if (!hour) return;
-
   const isNumber = hour.slice(-1) >= '0' && hour.slice(-1) <= '9';
 
   const isHour = (parseInt(hour, 10) >= 0 && parseInt(hour, 10)) < 24;
