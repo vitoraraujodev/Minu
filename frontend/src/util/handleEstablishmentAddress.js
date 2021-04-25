f54b47cc47290eeb70b6c92b855dc2252a6baca7
Vitor Araujo <vitoraraujo.dev@gmail.com>
2021-04-25 17:59:19 -0300

    Fix Address bug

--- frontend/src/util/handleEstablishmentAddress.js
+++ frontend/src/util/handleEstablishmentAddress.js
@@ -1,6 +1,8 @@
 // Rua São Clemente, 114 702 bloco 1
 
 export default function handleEstablishmentAddress(address = {}) {
+  if (!address) return 'Endereço indisponível';
+
   const complementText = address.complement ? `, ${address.complement}` : '';
 
   return `${address.street}, ${address.number}${complementText}`;
